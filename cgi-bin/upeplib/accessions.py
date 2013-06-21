25f31ecdc7db4f37762ba1ce3f31316f50488714
Mitchell Stanton-Cook <m.stantoncook@uq.edu.au>
2013-06-21 13:01:54 +1000

    Refactored ACLoc & GILoc methods & added debugging calls

--- cgi-bin/upeplib/accessions.py
+++ cgi-bin/upeplib/accessions.py
@@ -1,4 +1,6 @@
 import gzip
+import sys
+import os
 
 def seqtocaps(seq):
     temp = ''
@@ -11,60 +13,91 @@ def seqtocaps(seq):
 def ACCloc(accession):
     filename = 'ACCcompletecompact.dict.rna.gbff'
     filepath = '../RefSeq/' + filename
+    if not os.path.isfile(filepath):
+        sys.stderr.write("Debug 5: No file\n")
+        sys.stderr.flush()
     readfile = open(filepath,'rb')
-    readfile.seek(0, 2)
-    line = readfile.readline()
-    low = 0
-    high = int((readfile.tell()/80))
-    pos = (low+high)/2
-    while True:
-        oldpos = pos
-        readfile.seek(pos*80)
-        line = readfile.readline()
-        if accession < line.rsplit('\t')[0]:
-            high = pos
-            pos = (low+high)/2
-        elif accession > line.rsplit('\t')[0]:
-            low = pos
-            pos = (low+high)/2
-        else:
-            return line.rstrip().split('\t')
-        if pos == oldpos:
-            return None
+    return_val = None 
+    for line in readfile:
+        if line.find(accession) != -1:
+            return_val = line.rstrip().split('\t')
+            break
+    #readfile.seek(0, 2)
+    #line = readfile.readline()
+    #sys.stderr.write("Debug 4: "+str(line)+'\n')
+    #sys.stderr.flush()
+    #low = 0
+    #high = int((readfile.tell()/80))
+    #pos = (low+high)/2
+    #while True:
+    #    oldpos = pos
+    #    readfile.seek(pos*80)
+    #    line = readfile.readline()
+    #    if accession < line.rsplit('\t')[0]:
+    #        high = pos
+    #        pos = (low+high)/2
+    #    elif accession > line.rsplit('\t')[0]:
+    #        low = pos
+    #        pos = (low+high)/2
+    #    else:
+    #        return_val =  line.rstrip().split('\t')
+    #        break
+    #    if pos == oldpos:
+    #        return_val = None
+    #        break
     readfile.close()
+    sys.stderr.write("Debug 6: "+str(return_val)+'\n')
+    sys.stderr.flush()
+    return return_val
 
 def GIloc(accession):
     compare = str(accession)
     filename = 'GIcompletecompact.dict.rna.gbff'
     filepath = '../RefSeq/' + filename
     readfile = open(filepath,'rb')
-    readfile.seek(0, 2)
-    line = readfile.readline()
-    low = 0
-    high = int((readfile.tell()/40))
-    pos = (low+high)/2
-    while True:
-        oldpos = pos
-        readfile.seek(pos*40)
-        line = readfile.readline()
-        compareto = line.rsplit('\t')[0]
-        if compare < compareto:
-            high = pos
-            pos = (low+high)/2
-        elif compare > compareto:
-            low = pos
-            pos = (low+high)/2
-        else:
-            return ACCloc(line.rsplit('\t')[1].rstrip())
-        if pos == oldpos:
-            return None
-    readfile.close()    
+    return_val = None
+    for line in readfile:
+        if line.find(accession) != -1:
+            return_val = line.rstrip().split('\t')
+            break
+    #readfile.seek(0, 2)
+    #line = readfile.readline()
+    #low = 0
+    #high = int((readfile.tell()/40))
+    #pos = (low+high)/2
+    #while True:
+    #    oldpos = pos
+    #    readfile.seek(pos*40)
+    #    line = readfile.readline()
+    #    compareto = line.rsplit('\t')[0]
+    #    if compare < compareto:
+    #        high = pos
+    #        pos = (low+high)/2
+    #    elif compare > compareto:
+    #        low = pos
+    #        pos = (low+high)/2
+    #    else:
+    #        return ACCloc(line.rsplit('\t')[1].rstrip())
+    #    if pos == oldpos:
+    #        return None
+    readfile.close()
+    sys.stderr.write("Debug 7: "+str(return_val)+'\n')
+    sys.stderr.flush()
+    return return_val
+
+   
 
 def getCDS(accession):
+    sys.stderr.write("Debug 2: "+str(accession)+'\n')
+    sys.stderr.flush()
     if len(accession) > 3:
         if accession[:3] == 'GI:':
+            sys.stderr.write("Debug 3: GI \n")
+            sys.stderr.flush()
             return GIloc(accession[3:])
         elif (accession[:3] == 'NM_' or accession[:3] == 'XM_'):
+            sys.stderr.write("Debug 3: AC \n")
+            sys.stderr.flush()
             return ACCloc(accession)
         else:
             return None
@@ -74,6 +107,8 @@ def getCDS(accession):
 def getmRNA(accession, minsize, maxsize, gracelength):
     takejoins = True
     details = getCDS(accession)
+    sys.stderr.write("Debug 1: "+str(details)+'\n')
+    sys.stderr.flush()
     if details:
         handle = gzip.open('../RefSeq/' + details[2],'rb','9')
         try:
@@ -136,8 +171,3 @@ def getmRNA(accession, minsize, maxsize, gracelength):
         finally:
             handle.close()                           
     return None
-
-
-
-
-
